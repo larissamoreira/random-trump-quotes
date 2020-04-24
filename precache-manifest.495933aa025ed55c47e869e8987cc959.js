e27dee863241eb5f3b2f6589f5ac2847c0892ffc
Larissa Gusmao <larissamgusmao@gmail.com>
2020-04-23 22:00:21 -0400

    Updates

---
+++ precache-manifest.495933aa025ed55c47e869e8987cc959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38980558fa413158ef9b59cd79f95f9e",
+    "url": "/random-trump-quotes/index.html"
+  },
+  {
+    "revision": "bc4a93487a19827b3990",
+    "url": "/random-trump-quotes/static/css/main.a7d1843d.chunk.css"
+  },
+  {
+    "revision": "1be07dff9aa858bacb4a",
+    "url": "/random-trump-quotes/static/js/2.42042694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-trump-quotes/static/js/2.42042694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4a93487a19827b3990",
+    "url": "/random-trump-quotes/static/js/main.e08cfa87.chunk.js"
+  },
+  {
+    "revision": "570c8aed8e02e61326ab",
+    "url": "/random-trump-quotes/static/js/runtime-main.4ec88cf2.js"
+  },
+  {
+    "revision": "ce57c538ba05ac4681b7121e102b4331",
+    "url": "/random-trump-quotes/static/media/trump.ce57c538.png"
+  }
+]);
